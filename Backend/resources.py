c103280a6eca4d2d258114828fb486389fe5c1f9
Bjornar97 <hetlest@hotmail.com>
2019-03-22 18:10:15 +0100

    Kopierte fra Programvareutvikling og endret på noen ting
    Løser issue: #1

---
+++ Backend/resources.py
@@ -0,0 +1,344 @@
+from flask_restful import Resource, reqparse
+from models import User, RevokedTokenModel
+import random
+import json
+from run import app
+
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+
+registration_parser = reqparse.RequestParser()
+registration_parser.add_argument('email', help = 'This field cannot be blank', required = True)
+registration_parser.add_argument('password', help = 'This field cannot be blank', required = True)
+registration_parser.add_argument('username', help = 'This field can be blank', required = False)
+registration_parser.add_argument('birthday', help = 'This field can be blank', required = False)
+registration_parser.add_argument('phone', help = 'This field can be blank', required = False)
+registration_parser.add_argument('city', help = 'This field can be blank', required = False)
+
+
+# Registration
+## URI: /registration
+class UserRegistration(Resource):
+    def post(self):
+        data = registration_parser.parse_args()
+
+        if not data["email"]:
+            return {'message': 'Email is required'}
+
+        if not data["password"]:
+            return {'message': 'Password is required'}
+
+        # Checking if the email is already in our database, returns message if it is. Countinues if not.
+        if User.find_by_email(data['email']):
+            return {'message': 'User {} already exists'. format(data['email'])}
+
+        # Hashing password as soon as possible, Please dont add anything between the line above and below this comment
+        data["password"] = User.generate_hash(data["password"])
+
+        #TODO: Improve this \/
+        uid = random.randint(10000000, 99999999)
+        while User.find_by_uid(uid):
+            if uid >= 99999999:
+                uid = 10000000
+            else:
+                uid += 1
+        
+        # Making a new model with the email and password provided
+        new_user = User(
+            user_id = uid,
+            user_email = data["email"],
+            user_password = data["password"],
+            first_name = data["first_name"],
+            last_name = data["last_name"],
+            user_birthday = data["birthday"],
+            user_phone = data["phone"],
+            city = data["city"]
+        )
+
+        try:
+            # Saving the new user to the database. the method is located in models.py
+            new_user.save_to_db()
+
+            # Making tokens so the User is logged in
+            access_token = create_access_token(identity = uid)
+            refresh_token = create_refresh_token(identity = uid)
+
+            return {
+                'message': 'User {} was created'.format( data['email']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 201
+        except Exception as err:
+            return {'message': 'Something went wrong', 
+                "error": str(err)
+                }, 500
+
+# Login
+## URI: /login
+class UserLogin(Resource):
+    def post(self):
+        data = registration_parser.parse_args()
+
+        # Finding User from the database
+        current_user = User.find_by_email(data['email'])
+        if not current_user:
+            return {'message': 'User {} doesn\'t exist'.format(data['email'])}
+        
+        # Checking password, if correct, it makes tokens to log the User in
+        if User.verify_hash(data["password"], current_user.user_password):
+            access_token = create_access_token(identity = current_user.user_id)
+            refresh_token = create_refresh_token(identity = current_user.user_id)
+            return {
+                'message': 'Logged in as {}'.format(current_user.user_email),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 202
+        else:
+            return {'message': 'Wrong email or password'}, 401
+
+#Logout access
+## URI: /logout/access
+class UserLogoutAccess(Resource):
+    # Requires a jwt object to run, which basically means that the User must be logged in to log out(which makes sense)
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti = jti)
+            revoked_token.add()
+            return {'message': 'Access token has been revoked'}, 200
+        except Exception as err:
+            return {'message': 'Something went wrong', 
+                "error": str(err)
+                }, 500
+
+# Logout refresh
+## URI: /logout/refresh
+class UserLogoutRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti = jti)
+            revoked_token.add()
+            return {'message': 'Refresh token has been revoked'}, 200
+        except Exception as err:
+            return {'message': 'Something went wrong', 
+                "error": str(err)
+                }, 500
+
+# Refresh access token with refresh token
+## URI: /token/refresh
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity = current_user)
+        return {'access_token': access_token}, 201
+
+
+
+#Only for testing purposes
+## TODO: (IMPORTANTE) Delete "AllUsers" class before production!
+class AllUsers(Resource):
+    def get(self):
+        if app.debug == True:
+            return User.return_all(), 200
+        else:
+            return "You are not allowed to get all customers", 403
+    
+    def delete(self):
+        if app.debug == True:
+            print("Got into delete")
+            return User.delete_all(), 200
+        else:
+            return "You are not allowed to delete all customers", 403
+
+
+# The following classes are for the Api
+
+edit_parser = reqparse.RequestParser()
+edit_parser.add_argument('password', help = 'This field can be blank', required = False)
+edit_parser.add_argument('first_name', help = 'This field can be blank', required = False)
+edit_parser.add_argument('last_name', help = 'This field can be blank', required = False)
+edit_parser.add_argument('birthday', help = 'This field can be blank', required = False)
+edit_parser.add_argument('phone', help = 'This field can be blank', required = False)
+edit_parser.add_argument('city', help = 'This field can be blank', required = False)
+
+## URI: /v1/user/edit
+class Edit(Resource):
+    @jwt_required
+    def post(self):
+        data = edit_parser.parse_args()
+        
+        # Getting the uid from the jwt.
+        current_user = get_jwt_identity()
+
+        # Getting the User from the database through the model in models.py
+        user_object = User.find_by_uid(current_user)
+        
+        # Hashing password as soon as possible, Please dont add anything between the line above and below this comment
+        data["password"] = User.generate_hash(data["password"])
+
+        # Checks if no object got returned in the query, then return 401 Unauthorized.
+        if user_object.user_id == None:
+            return {"message": "Invalid uid. The User doesn't exist in our database"}, 401
+
+        if data["password"]:
+            user_object.user_password = data["password"]
+        if data["first_name"]:
+            user_object.first_name = data["first_name"]
+        if data["last_name"]:
+            user_object.last_name = data["last_name"]
+        if data["birthday"]:
+            user_object.user_birthday = data["birthday"]
+        if data["phone"]:
+            user_object.user_phone = data["phone"]
+        if data["city"]:
+            user_object.city = data["city"]
+
+        try:
+            # Saving the new user to the database. the method is located in models.py
+
+            return {
+                'message': 'User {} was edited'.format(user_object.user_email),
+            }, 201
+        except Exception as err:
+            return {'message': 'Something went wrong', 
+                "error": str(err)
+                }, 500
+
+## URI: /v1/user/uid
+class GetUid(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            # Getting the uid from the jwt.
+            current_user = get_jwt_identity()
+
+            # Getting the User from the database through the model in models.py
+            user_object = User.find_by_uid(current_user)
+
+            # Checks if no object got returned in the query, then return 401 Unauthorized.
+            if user_object == None:
+                return {"message": "Invalid uid. The User doesnt exist in our database"}, 401
+
+            return {"message": "The uid was found", 
+                "uid": current_user
+                }, 202
+
+        except Exception as err:
+            return {"message": "Something went wrong on the server", 
+                "error": str(err)
+                }, 500
+
+## URI: /v1/user/email
+class GetEmail(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            # Getting the uid from the jwt.
+            current_user = get_jwt_identity()
+
+            # Getting the User from the database through the model in models.py
+            user_object = User.find_by_uid(current_user)
+
+            # Checks if no object got returned in the query, then return 401 Unauthorized.
+            if user_object == None:
+                return {"message": "Invalid uid. The User doesnt exist in our database"}, 401
+            
+            return {"message": "Email of the User was found", "email": user_object.user_email}, 202
+
+        except Exception as err:
+            return {"message": "Something went wrong on the server", 
+                "error": str(err)
+                }, 500
+
+
+## URI: /v1/user/phone
+class GetPhone(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            # Getting the uid from the jwt.
+            current_user = get_jwt_identity()
+
+            # Getting the user from the database through the model in models.py
+            user_object = User.find_by_uid(current_user)
+
+            # Checks if no object got returned in the query, then return 401 Unauthorized.
+            if user_object.user_id == None:
+                return {"message": "Invalid uid. The user doesn't exist in our database"}, 401
+            
+            return {"message": "Phone of the user was found", "phone": user_object.user_phone}, 202
+
+        except Exception as err:
+            return {"message": "Something went wrong on the server", 
+                "error": str(err)
+                }, 500
+
+## URI: /v1/user/name
+class GetName(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            # Getting the uid from the jwt.
+            current_user = get_jwt_identity()
+
+            # Getting the user from the database through the model in models.py
+            user_object = User.find_by_uid(current_user)
+
+            # Checks if no object got returned in the query, then return 401 Unauthorized.
+            if user_object == None:
+                return {"message": "Invalid uid. The user doesnt exist in our database"}, 401
+
+            return {"message": "Name of the user was found", 
+                "first_name": user_object.first_name, 
+                "last_name": user_object.last_name
+                }, 202
+
+        except Exception as err:
+            return {"message": "Something went wrong on the server", 
+                "error": str(err)
+                }, 500
+
+## URI: /v1/user/all
+class GetAll(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            # Getting the uid from the jwt.
+            current_user = get_jwt_identity()
+
+            # Getting the user from the database through the model in models.py
+            user_object = User.find_by_uid(current_user)
+
+            # Checks if no object got returned in the query, then return 401 Unauthorized.
+            if user_object.user_id == None:
+                return {"message": "Invalid uid. The user doesn't exist in our database"}, 401
+
+            if user_object.address_id:
+
+                return {"message": "user was found", 
+                    "uid": current_user,
+                    "email": user_object.user_email,
+                    "first_name": user_object.first_name, 
+                    "last_name": user_object.last_name,
+                    "birthday": user_object.user_birthday,
+                    "phone": user_object.user_phone,
+                    "city": user_object.city
+                    }, 202
+                
+            return {"message": "user was found", 
+                "uid": current_user,
+                "email": user_object.user_email,
+                "first_name": user_object.first_name, 
+                "last_name": user_object.last_name,
+                "birthday": user_object.user_birthday,
+                "phone": user_object.user_phone,
+                "city": None,
+                }, 202
+
+        except Exception as err:
+            return {"message": "Something went wrong on the server", 
+                "error": str(err)
+                }, 500
---
+++ Backend/run.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from passlib.context import CryptContext
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+#Getting enviroment variables to avoid having secrets in the code base
+EnvVars = os.environ
+
+#Checking if the Enviroment variables exist, and uses them to connect to database. If they were not found, uses default values
+mysqlAddress = "mysql+pymysql://root:" + EnvVars["MySQLPassword"] + "@db/user"
+
+if EnvVars["MySQLPassword"] == "":
+    print("WARNING: \"MySQLPassword\" Environment variable is not set. Please add the environment variable and restart your computer, see \"setting up dev\" on https://github.com/DAT210/user. MySQL Database now has no password")
+    
+api = Api(app)
+
+#Setting up sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = mysqlAddress
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#Adding secret
+if EnvVars["userecret"] == "":
+    app.config['SECRET_KEY'] = "default-secret"
+    print("WARNING: \"userecret\" environment variable is not set! See \"setting up dev\" at https://github.com/DAT210/user for information. Using the default secret(unsecure!)")
+else: 
+    app.config['SECRET_KEY'] = EnvVars["userecret"]
+
+
+
+jwt = JWTManager(app)
+
+#Setting up blacklist
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['JWT_ERROR_MESSAGE_KEY'] = "message"
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+
+# Setting up passlib
+## Making a CryptoContext which is used for password hashing
+pwd_context = CryptContext(
+    # Schemes secify which hashing algorithm(s) we use
+    ## We will use argon2, which is relatively new, and very secure. 
+    ## CryptContext has support for multiple hashes to be used like this: shcemes = ["default", "legacy_read_only"]
+    schemes = ["argon2"],
+
+    #Deprecated="auto" will mark all but the default hash as deprecated, so it can only be used for reading
+    deprecated="auto"
+)
+
+import views, models, resources
+
+# Adding routes
+## These are located in the resources file
+
+### user actions: 
+api.add_resource(resources.UserRegistration, '/v1/registration')
+api.add_resource(resources.UserLogin, '/v1/login')
+api.add_resource(resources.UserLogoutAccess, '/v1/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/v1/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/v1/token/refresh')
+
+#user API for getting information:
+api.add_resource(resources.AllUsers, '/v1/user')
+api.add_resource(resources.GetUid, '/v1/user/uid')
+api.add_resource(resources.GetEmail, '/v1/user/email')
+api.add_resource(resources.GetName, '/v1/user/name')
+api.add_resource(resources.GetAll, '/v1/user/all')
+api.add_resource(resources.GetPhone, '/v1/user/phone')
+api.add_resource(resources.Edit, '/v1/user/edit')
+
+if __name__ == '__main__':
+    app.run(debug=True)
